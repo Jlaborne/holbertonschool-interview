ec4ff97d745edb63cd5dd6e8cf0583d633bf648c
Jlaborne <laborne.j@hotmail.fr>
2025-01-26 14:31:38 +0100

    Added documentation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+Calculating the minimum number of operations
+to produce n H characters in the file.
+"""
+
 
 def minOperations(n):
+    """
+    Calculate the fewest number of operations to produce n H characters.
+    Returns 0 if n is impossible to achieve.
+    """
+    
     if n <= 1:
         return 0  # Impossible to achieve
     
