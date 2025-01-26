287953aa88bc823f24ad7d2600acf20786db6468
Jlaborne <laborne.j@hotmail.fr>
2025-01-26 14:34:12 +0100

    PEP8 validation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -10,17 +10,16 @@ def minOperations(n):
     Calculate the fewest number of operations to produce n H characters.
     Returns 0 if n is impossible to achieve.
     """
-    
     if n <= 1:
         return 0  # Impossible to achieve
-    
+
     operations = 0
     factor = 2  # Start with the smallest prime number
-    
+
     while n > 1:
         while n % factor == 0:
             operations += factor  # Add the factor to operations
             n //= factor  # Divide n by the factor
         factor += 1  # Move to the next possible factor
-    
+
     return operations
