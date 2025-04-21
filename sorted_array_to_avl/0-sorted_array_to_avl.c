5636d5ace90187c090672522f890d3ed6515174d
Joshua Laborne <laborne.j@hotmail.fr>
2025-04-21 10:31:42 +0200

    t0

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,40 +1,44 @@
-#include <stdlib.h>
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
- * sorted_array_to_avl - Builds an AVL tree from a sorted array
- * @array: Pointer to the first element of the array to be converted
- * @size: Number of elements in the array
- *
- * Return: Pointer to the root node of the created AVL tree, or NULL on failure
+ * build_avl - Recursive helper to build AVL tree
+ * @array: Pointer to the first element
+ * @start: Start index
+ * @end: End index
+ * @parent: Pointer to parent node
+ * Return: Pointer to new node
+ */
+avl_t *build_avl(int *array, int start, int end, avl_t *parent)
+{
+    avl_t *node;
+    int mid;
+
+    if (start > end)
+        return (NULL);
+
+    mid = (start + end) / 2;
+    node = malloc(sizeof(avl_t));
+    if (!node)
+        return (NULL);
+
+    node->n = array[mid];
+    node->parent = parent;
+    node->left = build_avl(array, start, mid - 1, node);
+    node->right = build_avl(array, mid + 1, end, node);
+
+    return (node);
+}
+
+/**
+ * sorted_array_to_avl - Converts sorted array to AVL
+ * @array: Pointer to array
+ * @size: Size of array
+ * Return: Root of AVL tree
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-	avl_t *root;
-	int middle;
-
-	if (size == 0)
-		return (NULL);
-
-	/* Allocate memory for new node */
-	root = malloc(sizeof(avl_t));
-
-	if (root == NULL)
-		return (NULL);
-
-	/* Find middle element of the array */
-	middle = size / 2;
-
-	/* Create root node */
-	root->n = array[middle];
-	root->left = sorted_array_to_avl(array, middle);
-	root->right = sorted_array_to_avl(array + middle + 1, size - middle - 1);
-	root->parent = NULL;
-
-	if (root->left != NULL)
-		root->left->parent = root;
-	if (root->right != NULL)
-		root->right->parent = root;
-
-	return (root);
+    if (!array || size == 0)
+        return (NULL);
+    return (build_avl(array, 0, size - 1, NULL));
 }
