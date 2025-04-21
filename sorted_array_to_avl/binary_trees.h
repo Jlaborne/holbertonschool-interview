042695d1a82ef6cdd5c3bfca1134a61a0bdb2105
Joshua Laborne <laborne.j@hotmail.fr>
2025-04-21 10:35:49 +0200

    t0

--- sorted_array_to_avl/binary_trees.h
+++ sorted_array_to_avl/binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stddef.h>
 
@@ -11,14 +11,17 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
-void binary_tree_print(const binary_tree_t *);
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t; /* <- Ce typedef est nécessaire pour avl_t */
 
-#endif /* _BINARY_TREES_H_ */
+avl_t *sorted_array_to_avl(int *array, size_t size); /* prototype */
+
+#endif /* BINARY_TREES_H */
