81cef59d563fb0b2f3a6acd34b3c1edd337ba07f
Jlaborne <laborne.j@hotmail.fr>
2025-08-29 11:32:42 +0200

    Fix (6, [1, 1, 0, 0, 1, 8])

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -25,38 +25,29 @@ def isWinner(x, nums):
         if nums[i] > max_n:
             max_n = nums[i]
 
-    if max_n < 2:
-        # No primes in any round
-        return None
-
-    # Sieve of Eratosthenes up to max_n
-    is_prime = [False, False] + [True] * (max_n - 1)  # index 0..max_n
+    # Sieve size covers indices 0..max_n
+    is_prime = [False, False] + [True] * max(0, max_n - 1)
     p = 2
     while p * p <= max_n:
         if is_prime[p]:
-            step = p
-            start = p * p
-            j = start
+            j = p * p
             while j <= max_n:
                 is_prime[j] = False
-                j += step
+                j += p
         p += 1
 
-    # Prefix count of primes: primes_count[n] = number of primes <= n
+    # Prefix counts of primes: primes_count[n] = #primes <= n
     primes_count = [0] * (max_n + 1)
-    c = 0
-    i = 0
-    while i <= max_n:
+    cnt = 0
+    for i in range(max_n + 1):
         if is_prime[i]:
-            c += 1
-        primes_count[i] = c
-        i += 1
+            cnt += 1
+        primes_count[i] = cnt
 
     maria = 0
     ben = 0
     for i in range(rounds):
         n = nums[i]
-        # If prime count up to n is odd => Maria wins this round, else Ben
         if primes_count[n] % 2 == 1:
             maria += 1
         else:
